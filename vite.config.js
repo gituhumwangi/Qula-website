ee793c8e95569dc5f6c6fa892c531ea941370e50
dennis <dennis.mwangi@student.moringaschool.com>
2024-06-05 10:02:54 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Replace 'your-repo-name' with the actual repository name
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/"
-})
+  base: 'Qula-website'
+});
