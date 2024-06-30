42f1dfc2e06b3af91a1f74ca6e60e87e0cb92902
dennis <dennis.mwangi@student.moringaschool.com>
2024-06-30 20:48:54 +0300

    Added splashes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // Replace 'your-repo-name' with the actual repository name
 export default defineConfig({
   plugins: [react()],
-  base: 'Qula-website'
+  base: '/Qula-website/'
 });
